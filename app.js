1c0d7ee4e44854702ba90d383d4d9ff787b84392
Nathaly V <166789478+Nathbecode@users.noreply.github.com>
2024-06-13 15:48:27 +0200

    finish

--- app.js
+++ app.js
@@ -135,29 +135,116 @@ document.getElementById('getMeal').addEventListener('click', getMeal);
 
 function getMeal() {
     let letre = document.getElementById('input').value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letre}`)
-    .then(function(res){
-        return res.json();
-    })
-    .then(function(data) {
-        let output = `<h2>The results for "${letre}"</h2>
-            <div class="container">`
-        data.meals.forEach(element => {
-            output += `
-            <div class="meal">
-                <ul>
-                    <li><img src="${element.strMealThumb}" class="imagen"></li>
-                    <li>${element.strMeal}</li>
-                    <li>Ingredient 1: ${element.strIngredient1}, ${element.strMeasure1}</li>
-                </ul>
-            </div>`
-        });
-        document.getElementById('output').innerHTML = output;
-        output += '</div>'
-        document.querySelectorAll('.imagen').forEach((ele)=> {
-            ele.style.width = '100%';
-            ele.style.transition= 'width 0.3s ease-in-out';
+    if(letre.length === 1){
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letre}`)
+        .then(function(res){
+            return res.json();
         })
-    })
-    .catch((err) => console.log(err));
-}
+        .then(function(data) {
+            let output = `<h2>The results for "${letre}"</h2>
+                <div class="container">`
+            data.meals.forEach(element => {
+                output += `
+                    <div class="meal" data-meal='${JSON.stringify(element)}'>
+                        <ul>
+                            <li><img src="${element.strMealThumb}" class="imagen"></li>
+                            <li>${element.strMeal}</li>
+                        </ul>
+                    </div>`
+            });
+            document.getElementById('output').innerHTML = output;
+            output += '</div>'
+            document.querySelectorAll('.imagen').forEach((ele)=> {
+                ele.style.width = '100%';
+                ele.style.transition= 'width 0.3s ease-in-out';
+            })
+            document.querySelector('.container').addEventListener('click',(e)=>{
+                const mealElement = e.target.closest('.meal');
+                if(mealElement){
+                    const mealData = JSON.parse(mealElement.getAttribute('data-meal'));
+                    showModal(mealData);}
+            })
+        })            
+        .catch((err) => console.log(err));
+    }  
+    else {
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${letre}`)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(data) {
+            let output = `<h2>The results for "${letre}"</h2>
+                <div class="container">`
+            data.meals.forEach(element => {
+                output += `
+                    <div class="meal" data-meal='${JSON.stringify(element)}'>
+                        <ul>
+                            <li><img src="${element.strMealThumb}" class="imagen"></li>
+                            <li>${element.strMeal}</li>
+                        </ul>
+                    </div>`;
+            });
+            document.getElementById('output').innerHTML = output;
+            output += '</div>'
+            document.querySelectorAll('.imagen').forEach((ele)=> {
+                ele.style.width = '100%';
+                ele.style.transition= 'width 0.3s ease-in-out';
+            })
+            document.querySelector('.container').addEventListener('click',(e)=>{
+                const mealElement = e.target.closest('.meal');
+                if(mealElement){
+                    const mealData = JSON.parse(mealElement.getAttribute('data-meal'));
+                    showModal(mealData)}})
+        })
+        .catch((err) => console.log(err));
+    }}
+
+    function showModal(mealData) {
+        let modal = document.getElementById("myModal");
+        let span = document.getElementsByClassName("close")[0];
+        let body = document.querySelector('body');
+        
+        let modalContent;
+
+    if (mealData === null) {
+        modalContent = `
+            <p></p>
+        `;
+    } else {
+        modalContent = `
+            <h2>${mealData.strMeal}</h2>
+            <img src="${mealData.strMealThumb}" class="img-modal">
+            <p><strong>Ingredients :</strong><br></p>
+            <ul>`;
+
+        for (let i = 1; i <= 20; i++) {
+            const ingredient = mealData[`strIngredient${i}`];
+            const measure = mealData[`strMeasure${i}`];
+            if (ingredient && measure) {
+                modalContent += `<li>${ingredient}, ${measure}</li>`;
+            }
+        }
+        modalContent += `
+            </ul> <br>
+            <p>${mealData.strInstructions.replace(/\r\n/g, '<br>')}</p>
+        `;
+    }
+
+        document.querySelector('.modal-content .mealcontent').innerHTML = modalContent;
+    
+        modal.style.display = "block";
+        body.style.position = 'fixed';
+    
+        span.onclick = function() {
+            modal.style.display = "none";
+            body.style.position = 'static';
+        }
+    
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                body.style.position = 'static';
+            }
+        }
+    }
+
